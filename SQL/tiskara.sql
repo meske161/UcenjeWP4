2a3b57964ee0bcf4a7f8c2080c3910af10f8d11c
meske161 <mesaritchievan@gmail.com>
2024-05-14 18:45:51 +0200

    Vjezba1

---
+++ SQL/tiskara.sql
@@ -0,0 +1,16 @@
+﻿use master;
+go
+drop database if exists tiskara
+go
+create database tiskara
+go
+
+use tiskara
+create table strojevi (
+sifra int not null primary key identity (1,1),
+proizvodnja varchar (30) not null,
+dorada varchar (30) not null, 
+radionica varchar (30) not null, 
+tisak varchar (30) not null, 
+maloprodaja varchar (30) not null
+);
